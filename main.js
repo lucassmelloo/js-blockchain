00e525c6b806b6314e644858490dfa0a745787c2
Mello <lucasmellovieira99@gmail.com>
2021-09-29 21:29:33 -0300

    Testing Blockchain

--- main.js
+++ main.js
@@ -2,16 +2,16 @@ const SHA256 = require('crypto-js/sha256')
 
 
 class Block{
-    constructor(blockIndex, blockCreationDate, blockInformation, blockPreviousHash = ''){
-        this.blockIndex = blockIndex;
-        this.blockCreationDate = blockCreationDate;
-        this.blockInformation = blockInformation;
-        this.blockPreviousHash = blockPreviousHash;
-        this.blockCurrentHash = this.calculateHash();
+    constructor(indexOfBlock, creationDate, informationInBlock, previusHash = ''){
+        this.indexOfBlock = indexOfBlock;
+        this.creationDate = creationDate;
+        this.informationInBlock = informationInBlock;
+        this.previusHash = previusHash;
+        this.currentHash = this.calculateHash();
     }
 
     calculateHash(){
-        return SHA256(this.blockIndex + this.blockPreviousHash + this.blockCreationDate + JSON.stringify(this.blockInformation)).toString();
+        return SHA256(this.indexOfBlock + this.previusHash + this.creationDate + JSON.stringify(this.informationInBlock)).toString();
     }
 }
 
@@ -29,16 +29,25 @@ class Blockchain{
     }
 
     addBlock(newBlock){
-        newBlock.blockPreviousHash = this.getLatestBlock().blockCurrentHash;
-        newBlock.blockCurrentHash = newBlock.calculateHash();
+        newBlock.previusHash = this.getLatestBlock().currentHash;
+        newBlock.currentHash = newBlock.calculateHash();
         this.chain.push(newBlock);
     }
 
     isChainValid(){
         for(let i = 1; i < this.chain.length; i++){
-            const blockCurrentBlock = this.chain[i];
-            const previusBlock = this.chain[i-1];
+            const currentBlock = this.chain[i];
+            const previusBlock = this.chain[i - 1];
+
+            if(currentBlock.currentHash !== currentBlock.calculateHash()){
+                return false;
+            }
+
+            if(currentBlock.previusHash !== previusBlock.currentHash){
+                return false;
+            }
         }
+        return true;
     }
 }
 
@@ -46,4 +55,14 @@ let melloCoin = new Blockchain();
 melloCoin.addBlock(new Block(1, '28/09/2021', { amount: 4}));
 melloCoin.addBlock(new Block(2, '29/09/2021', { amount: 10}));
 
-console.log(JSON.stringify(melloCoin, null, 4));
+console.log(JSON.stringify(melloCoin,null,4))
+
+console.log('Essa cadeia de blocos é valida?: ' + melloCoin.isChainValid());
+
+melloCoin.chain[1].data = {amount: 100};
+console.log(JSON.stringify(melloCoin,null,4))
+
+console.log(melloCoin.chain[1].previusHash + "             " + melloCoin.chain[1].indexOfBlock);
+console.log(melloCoin.chain[0].currentHash + "             " + melloCoin.chain[0].indexOfBlock);
+console.log('Essa cadeia de blocos é valida?: ' + melloCoin.isChainValid());
+//console.log(JSON.stringify(melloCoin, null, 4));
