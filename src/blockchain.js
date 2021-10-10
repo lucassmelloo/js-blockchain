0825a4b7b6e96f7d7791fe1fb37d68a7a0941d4f
Mello <lucasmellovieira99@gmail.com>
2021-10-09 23:19:24 -0300

    Funções das transactions

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,4 +1,6 @@
-const SHA256 = require('crypto-js/sha256')
+const SHA256 = require('crypto-js/sha256');
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
 
 class Transaction{
     constructor (fromAddress, toAddress, amount){
@@ -19,6 +21,17 @@ class Transaction{
         const hashTx = this.calculateHash();
         const sig = signingKey.sign(hashTx, 'base64');
         this.signature = sig.toDER('hex');
+    }
+
+    isValid(){
+        if(this.fromAddress === null) return true;
+
+        if(!this.signature || this.signature.length === 0){
+            throw new Error('No Signature in this transaction');
+        }
+
+        const publicKey = ec.keyFromPublic(this.fromAddress,'hex');
+        return publicKey.verify(this.calculateHash(),this.signature);
     }
 }
 
@@ -42,6 +55,16 @@ class Block{
         }
 
         console.log("Block mined: " + this.currentHash);
+    }
+
+    hasValidTransactions(){
+        for(const tx of this.transactions){
+            if(!tx.isValid()){
+                return false;
+            }
+        }
+
+        return true;
     }
 }
 
